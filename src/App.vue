731c04c61a2b7c8e011d790d49d4398163445786
Carolina Gonzalez <c.gonzalezcontrera@gmail.com>
2021-04-03 19:31:23 -0300

    agregando cronometro

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,92 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <h1>Cuenta Regresiva</h1>
+      <h2>{{ formatoTiempo(tiempo) }}</h2>
+      <button v-for="valores in botones" :key="valores" @click="countdown(valores.tiempo)">{{valores.etiqueta}}</button>
+      <br>
+        <div v-if="tiempo > 0">
+          <button @click="pausar(estado.activo)">{{estado.boton}}</button>
+          <button @click="tiempo = 0">Reset</button>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  name: 'Fechas',
+  data(){
+    return{
+      tiempo : 0,
+        botones: [
+            {etiqueta: '03s'  , tiempo: 3 },
+            {etiqueta: '01m'  , tiempo: 60 }, 
+            {etiqueta: '05m'  , tiempo: 300 }, 
+            {etiqueta: '10m' , tiempo: 600 }, 
+            {etiqueta: '30m' , tiempo: 1800 },  
+        ],
+        intervalo : '',
+     estado: {activo: false, boton: 'Iniciar'},
+    }
   },
-};
+  methods: {
+      formatoTiempo: function (seg){
+          let minutos = ('0' + Math.floor(seg/60)).slice(-2);
+          let segundos = ('0' + seg%60).slice(-2);
+          return `${minutos}:${segundos} min/seg`
+      },
+      countdown: function (count){
+          clearInterval(this.intervalo)
+          this.tiempo = count;
+            this.estado.activo = true;
+          this.estado.boton = 'Pausar';
+          this.intervalo = setInterval(()=>{
+              if (this.tiempo > 0) {
+                  this.tiempo--; 
+              } else {
+                  clearInterval(this.intervalo)
+              }
+          } , 1000);
+          
+      },
+        pausar: function(active){
+          if(active){
+              clearInterval(this.intervalo);
+              this.estado.activo = false;
+              this.estado.boton = 'Iniciar';
+          }else if(this.tiempo > 0){
+              this.countdown(this.tiempo);
+          }
+      }
+  },
+}
+
 </script>
 
+
 <style lang="scss">
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+  body{
+      text-align: center;
+     
+  }
+  h1{
+      font-size: 130px;
+  }
+  h2{
+      font-size: 90px;
+      color: gray;
+  }
+  button{
+      margin: 20px;
+      font-size: 50px;
+  }
+
 </style>
